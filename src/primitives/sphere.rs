607583c0d60a8e5ec391e40d00c833059ed0bab0
simluyt <=>
2021-02-09 20:54:35 +0100

    RC update

--- src/primitives/sphere.rs
+++ src/primitives/sphere.rs
@@ -40,7 +40,7 @@ impl Hittable for Sphere {
         rec.p = r.at(rec.t);
         let outward_normal = (rec.p -self.center) / self.radius;
         rec.set_face_normal(r, outward_normal);
-        rec.material = Box::new(*self.material);
+        rec.material = self.material.clone();
 
         true
     }
